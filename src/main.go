32578a1dad96693964c114b6473279a44bef4039
= <=>
2026-01-21 23:06:25 +0500

    add art and fix random number generation

--- src/main.go
+++ src/main.go
@@ -12,28 +12,58 @@ import (
 
 const wordsFilePath string = "resources/words.txt"
 
-func startGame() error {
-	word, err := readWordFromFile()
-	if err != nil {
-		return errors.New("failed to open file")
+func main() {
+	var input string
+
+	fmt.Print("Would you like to start a new game? (y/n): ")
+	_, err := fmt.Scanf("%s", &input)
+	for err != nil || !isCorrectInput(input) || !strings.ContainsAny(input, "yn") {
+		fmt.Println("Wrong input! try again!")
+		_, err = fmt.Scanf("%s", &input)
 	}
+
+	input = strings.ToLower(input)
+	switch input {
+	case "y":
+		word, err := readWordFromFile()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		startGame(word)
+	case "n":
+		break
+	}
+}
+
+func isCorrectInput(input string) bool {
+	return len(input) == 1
+}
+
+func startGame(word string) {
 	var hint []rune
 
 	for i := 0; i < len(word); i++ {
 		hint = append(hint, '_')
 	}
 	var guess rune
+	var guessStr string
 	mistakes := 0
 
 	for slices.Contains(hint, '_') && mistakes < 6 {
 		fmt.Println(string(hint))
 		fmt.Printf("Mistakes: %d", mistakes)
+		fmt.Println(art[mistakes])
 
 		fmt.Print("guess?: ")
 
-		_, err = fmt.Scanf("%c\n", &guess)
+		_, err := fmt.Scanln(&guessStr)
 		if err != nil {
 			panic("error reading from stdin")
+		}
+		for _, char := range guessStr {
+			guess = char
+			break
 		}
 
 		if strings.ContainsRune(word, guess) {
@@ -47,8 +77,10 @@ func startGame() error {
 		} else {
 			mistakes++
 		}
+		if mistakes == 6 {
+			fmt.Println(art[6])
+		}
 	}
-	return nil
 }
 
 func readWordFromFile() (string, error) {
@@ -60,31 +92,12 @@ func readWordFromFile() (string, error) {
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	i := true
+	counter := 0
 	for i {
+		counter++
 		i = scanner.Scan()
 		lines = append(lines, scanner.Text())
 	}
-	randInt := rand.Int() % len(lines)
+	randInt := rand.IntN(counter - 1)
 	return lines[randInt], nil
-}
-
-func main() {
-	var input string
-	fmt.Println("Would you like to start a new game? (y/n)")
-	_, err := fmt.Scanf("%s", &input)
-	for err != nil {
-		fmt.Println("Wrong input! try again!")
-		_, err = fmt.Scanf("%d", &input)
-	}
-	switch input {
-	case "Y":
-	case "y":
-		err := startGame()
-		if err != nil {
-			return
-		}
-	case "N":
-	case "n":
-		break
-	}
 }
